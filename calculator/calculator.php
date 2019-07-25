431394b4309cacfb8bc9c873ac7b337ad6274a49
Franz <franz.delatorre@fullspeedtechnologies.com>
2019-07-25 18:18:31 +0800

    123

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -4,10 +4,12 @@
         do{
             $operator = readline("Select Operator (ADD,SUB,DIVIDE,MULTIPLY,EXIT): ");
             $opt = strtoupper($operator);
-            if($opt == "ADD" || $opt == "SUB")
+            if($opt == "ADD" || $opt == "SUB" || $opt == "DIVIDE" || $opt == "MULTIPLY" || $opt == "EXIT")
                 $checker = 1;
-            else    
+            else{
                 echo "INVALID OPERATOR!!!\n";
+                unset($opt);
+            }
         }while($checker == 0);
         
         if($opt == "EXIT"){
@@ -19,13 +21,13 @@
         $val2 = readline("Input Second Value: ");
         
         if($opt == "ADD")
-            echo "The Sum of the 2 numbers is ".($val1+$val2."\n");
+            echo "$val1 + $val2 = ".($val1+$val2."\n");
         elseif($opt == "SUB")
-            echo "The Difference of the 2 numbers is ".($val1-$val2."\n");
+            echo "$val1 - $val2 =".($val1-$val2."\n");
         elseif($opt == "DIVIDE")
-            echo "The Quotient of the 2 numbers is ".($val1/$val2."\n");
+            echo "$val1 / $val2 =".($val1/$val2."\n");
         elseif($opt == "MULTIPLY")
-            echo "The Product of the 2 numbers is ".($val1*$val2."\n");
-        else echo "INVALID OPERATOR! \n";
+            echo "$val1 * $val2 =".($val1*$val2."\n");
+        
     }while($checker = 1);
 ?>
