7bdbeaa95fff3ac4c346bafae13f9a6de53f59b1
Franz <franz.delatorre@fullspeedtechnologies.com>
2019-07-26 14:59:37 +0800

    gasg

--- palindrome/palindrome.php
+++ palindrome/palindrome.php
@@ -2,9 +2,7 @@
     $input = readline("Enter String: ");
     $revinput = strrev($input);
     $checker = true;
-    if($input>=00 || $input<0)
-        echo "Not a Palindrome!\n";
-    else{
+    
         for($i = strlen($input)/2 ; $i > 0 ; $i--){
             if($input[$i] != $revinput[$i]) {
                 echo "Not a Palindrome!\n";
@@ -13,5 +11,6 @@
             }
         }
         if($checker) echo "Palindrome!\n";
-    }
+    
+    
 ?>
