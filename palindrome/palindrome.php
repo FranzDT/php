9f04d76373fbedd6dd4f191eeb4dd97e6c19caae
Franz <franz.delatorre@fullspeedtechnologies.com>
2019-07-26 14:53:45 +0800

    newnew

--- palindrome/palindrome.php
+++ palindrome/palindrome.php
@@ -2,13 +2,16 @@
     $input = readline("Enter String: ");
     $revinput = strrev($input);
     $checker = true;
-    for($i = strlen($input)/2 ; $i > 0 ; $i--){
-        if($input[$i] != $revinput[$i]) {
-            echo "Not a Palindrome!\n";
-            $checker = false;
-            break;
+    if($input>=00 || $input<0)
+        echo "Not a Palindrome!\n";
+    else{
+        for($i = strlen($input)/2 ; $i > 0 ; $i--){
+            if($input[$i] != $revinput[$i]) {
+                echo "Not a Palindrome!\n";
+                $checker = false;
+                break;
+            }
         }
+        if($checker) echo "Palindrome!\n";
     }
-    if($checker) echo "Palindrome!\n";
-
 ?>
--- pyramid/pyramid.php
+++ pyramid/pyramid.php
@@ -2,11 +2,12 @@
     $y = readline("Input: ");
     $ctr = 1;
     for($i = $y ; $i > 0 ; $i--){
-        for($h=$i;$h>0;$h--){
+        
+        for($h=$i;$h>0;$h--){ //sets the whitespaces 
             echo " ";
         }
         for($j = 0 ; $j < $ctr ; $j++ ){
-            echo "* ";
+            echo "* "; //sets the amount of asterisk
         }
         echo "\n";
         $ctr++;
